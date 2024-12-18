29c8e8f0c59cef364807be7cea0e8a231775dd71
yun010207 <yun010207@naver.com>
2024-12-18 12:32:38 +0900

    id역순으로 article가져옴

--- src/main/java/com/LocalBusan/LocalBusan/Article/ArticleRepository.java
+++ src/main/java/com/LocalBusan/LocalBusan/Article/ArticleRepository.java
@@ -1,11 +1,7 @@
 package com.LocalBusan.LocalBusan.Article;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
     // 추가적인 쿼리 메서드 작성 가능
-    @Query("SELECT * FROM Article ORDER BY DESC")
-    List<Article> findAllOrderByIdDesc();
 }
