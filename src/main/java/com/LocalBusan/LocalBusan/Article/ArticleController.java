f4156ef4c8e311ee76a4e8cd4297b152fbfd7307
yun010207 <yun010207@naver.com>
2024-12-18 12:43:14 +0900

    id역순으로 article가져옴

--- src/main/java/com/LocalBusan/LocalBusan/Article/ArticleController.java
+++ src/main/java/com/LocalBusan/LocalBusan/Article/ArticleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@RequestMapping("/api/board")
+@RequestMapping("/api/boards")
 @RestController
 public class ArticleController {
 
