2b286ccee35712b40e3651e2d227a2254f993f33
yun010207 <yun010207@naver.com>
2024-12-18 13:06:43 +0900

    id역순으로 article가져옴

--- src/main/java/com/LocalBusan/LocalBusan/Article/ArticleService.java
+++ src/main/java/com/LocalBusan/LocalBusan/Article/ArticleService.java
@@ -95,7 +95,7 @@ public class ArticleService {
 
     // 게시글 목록 조회
     public List<Article> getAllArticles() {
-        return articleRepository.findAll(Sort.by(Sort.Direction.DESC, "title"));
+        return articleRepository.findAll(Sort.by(Sort.Direction.DESC, "article_id"));
     }
     // 게시글 조회 (ID로 검색)
     public Article getArticleById(Integer articleId) {
